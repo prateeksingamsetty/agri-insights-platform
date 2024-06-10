cd69bd38b5efe92e4e314046c8b7aec8a9de2dc5
Prateek Kumar Singamsetty <pksingam@ncsu.edu>
2024-06-10 16:48:00 +0530

    py vers change in yml and chng in code

--- DataScraping/Tomato/tomatoPrices.py
+++ DataScraping/Tomato/tomatoPrices.py
@@ -4,6 +4,7 @@ import time
 import platform
 from datetime import date, timedelta
 
+import certifi
 import pandas as pd
 import pymongo
 from selenium import webdriver
@@ -20,10 +21,8 @@ from selenium.common.exceptions import (
 
 # Constants
 if platform.system() == 'Windows':
-    CHROME_DRIVER_PATH = "C:\Program Files (x86)\chromedriver.exe"
     DOWNLOAD_PATH = "C:\\Users\\hp\\Downloads"
 elif platform.system() == 'Linux':
-    CHROME_DRIVER_PATH = "/usr/local/bin/chromedriver"
     DOWNLOAD_PATH = "/home/runner/downloads"
 else:
     raise OSError("Unsupported operating system.")
@@ -31,22 +30,27 @@ else:
 mongodb_username = os.getenv("MONGODB_USERNAME")
 mongodb_password = os.getenv("MONGODB_PASSWORD")
 
-MONGODB_CONNECTION_STRING = f"mongodb+srv://{mongodb_username}:{mongodb_password}@cluster0.bdxk2dg.mongodb.net/"
+# MONGODB_CONNECTION_STRING = f"mongodb+srv://{mongodb_username}:{mongodb_password}@cluster0.bdxk2dg.mongodb.net/"
+MONGODB_CONNECTION_STRING = "mongodb://localhost:27017/"
 
 # Functions
+
 
 def wait_and_click(driver, locator):
     """Wait for element to be clickable and click."""
     wait.until(EC.element_to_be_clickable(locator)).click()
+
 
 def select_dropdown_option(main_div_id, option_to_be_selected):
     """Select option from dropdown."""
     retries = 3
     while retries > 0:
         try:
-            div_element = wait.until(EC.element_to_be_clickable((By.ID, main_div_id)))
+            div_element = wait.until(
+                EC.element_to_be_clickable((By.ID, main_div_id)))
             div_element.click()
-            dropdown_options = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, f"#{main_div_id} .chosen-results li")))
+            dropdown_options = wait.until(EC.visibility_of_all_elements_located(
+                (By.CSS_SELECTOR, f"#{main_div_id} .chosen-results li")))
             for option in dropdown_options:
                 if option.text == option_to_be_selected:
                     option.click()
@@ -56,6 +60,7 @@ def select_dropdown_option(main_div_id, option_to_be_selected):
             retries -= 1
             time.sleep(2)
 
+
 def select_multiple_dropdown_options(main_div_id, options_to_be_selected):
     """Select multiple options from dropdown."""
     options_to_select = list(options_to_be_selected.keys())
@@ -64,9 +69,11 @@ def select_multiple_dropdown_options(main_div_id, options_to_be_selected):
 
     while selected_options < len(options_to_select):
         try:
-            div_element = wait.until(EC.element_to_be_clickable((By.ID, main_div_id)))
+            div_element = wait.until(
+                EC.element_to_be_clickable((By.ID, main_div_id)))
             div_element.click()
-            dropdown_options = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, f"#{main_div_id} .chosen-results li")))
+            dropdown_options = wait.until(EC.visibility_of_all_elements_located(
+                (By.CSS_SELECTOR, f"#{main_div_id} .chosen-results li")))
 
             for option in dropdown_options:
                 if option.text in options_to_be_selected and options_to_be_selected[option.text]:
@@ -83,6 +90,7 @@ def select_multiple_dropdown_options(main_div_id, options_to_be_selected):
                 raise
             time.sleep(2)
 
+
 def click_button_with_retry(button_locator, retries=3):
     """Click a button with retry logic."""
     while retries > 0:
@@ -96,6 +104,7 @@ def click_button_with_retry(button_locator, retries=3):
                 raise
             time.sleep(2)
 
+
 def convert_csv_to_json(download_path):
     """Convert CSV file to JSON."""
     csv_files = glob.glob(os.path.join(download_path, "Shipping*.csv"))
@@ -104,17 +113,19 @@ def convert_csv_to_json(download_path):
         return
 
     latest_csv_file = max(csv_files, key=os.path.getctime)
-    print(latest_csv_file)  
+    print(latest_csv_file)
 
     df = pd.read_csv(latest_csv_file)
     json_data = df.to_dict(orient='records')
 
     return json_data
 
+
 def push_data_to_mongodb(data, db_name, collection_name, connection_string):
     """Push data to MongoDB."""
     try:
-        client = pymongo.MongoClient(connection_string)
+        client = pymongo.MongoClient(
+            connection_string, tlsCAFile=certifi.where())
         db = client[db_name]
         collection = db[collection_name]
         collection.insert_many(data)
@@ -128,6 +139,7 @@ def push_data_to_mongodb(data, db_name, collection_name, connection_string):
 
 # Main script
 
+
 options = webdriver.ChromeOptions()
 prefs = {"download.default_directory": "/home/runner/downloads"}  # Adjust path
 options.add_experimental_option("prefs", prefs)
@@ -137,7 +149,6 @@ if platform.system() == 'Linux':
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
 
-# driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH, options=options)
 driver = webdriver.Chrome(options=options)
 wait = WebDriverWait(driver, 20)
 
@@ -147,24 +158,30 @@ try:
     market_tab = wait.until(EC.element_to_be_clickable((By.ID, "market-tab")))
     market_tab.click()
 
-    select_dropdown_option("market_types_by_market_types_chosen", "Shipping Point")
+    select_dropdown_option(
+        "market_types_by_market_types_chosen", "Shipping Point")
     select_dropdown_option("drill_types_chosen", "Location - State")
-    select_dropdown_option("aggregate_by_market_types_section_2_chosen", "Weekly")
+    select_dropdown_option(
+        "aggregate_by_market_types_section_2_chosen", "Weekly")
 
-    input_date_element = wait.until(EC.presence_of_element_located((By.ID, "from-date-by-market-types-section-2")))
+    input_date_element = wait.until(EC.presence_of_element_located(
+        (By.ID, "from-date-by-market-types-section-2")))
     today = date.today()
     today_str = today.strftime("%m/%d/%Y")
     input_date_element.send_keys(today_str)
     input_date_element.send_keys(Keys.RETURN)
 
-    input_date_element = wait.until(EC.presence_of_element_located((By.ID, "to-date-by-market-types-section-2")))
+    input_date_element = wait.until(EC.presence_of_element_located(
+        (By.ID, "to-date-by-market-types-section-2")))
     seven_days_ago = today - timedelta(days=6)
     seven_days_ago_str = seven_days_ago.strftime("%m/%d/%Y")
     input_date_element.send_keys(seven_days_ago_str)
     input_date_element.send_keys(Keys.RETURN)
 
-    select_multiple_dropdown_options("commodities_by_market_types_section_2_chosen", {"Tomatoes": True})
-    select_multiple_dropdown_options("locations_by_market_types_section_2_chosen", {"Atlanta (Forest Park), Georgia": True, "Columbia, South Carolina": True, "Miami, Florida": True, "Orlando (Oviedo), Florida": True, "Raleigh, North Carolina": True})
+    select_multiple_dropdown_options(
+        "commodities_by_market_types_section_2_chosen", {"Tomatoes": True})
+    select_multiple_dropdown_options("locations_by_market_types_section_2_chosen", {
+                                     "Atlanta (Forest Park), Georgia": True, "Columbia, South Carolina": True, "Miami, Florida": True, "Orlando (Oviedo), Florida": True, "Raleigh, North Carolina": True})
 
     click_button_with_retry((By.ID, "btnGetDataMarketType"))
     click_button_with_retry((By.CSS_SELECTOR, '.btn-save-as-csv'))
@@ -172,7 +189,8 @@ try:
     time.sleep(5)
 
     json_data = convert_csv_to_json(DOWNLOAD_PATH)
-    push_data_to_mongodb(json_data, "Agri_Insights", "tomatoPrices", MONGODB_CONNECTION_STRING)
-    
+    push_data_to_mongodb(json_data, "Agri_Insights",
+                         "tomatoPrices", MONGODB_CONNECTION_STRING)
+
 finally:
     driver.quit()
